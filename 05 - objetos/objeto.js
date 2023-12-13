19f1a77d936d6fb44af0dfbe3a571591b853ce57
tocattabrito <tocattabrito@gmail.com>
2023-12-12 21:29:36 -0300

    Objetos

---
+++ 05 - objetos/objeto.js
@@ -0,0 +1,11 @@
+
+// chave e valor
+const brito = {
+    nome: 'João Brito',
+    idade: 43
+};
+
+//acessando os dados da pessoa
+console.log(brito.nome);
+console.log(brito.idade);
+console.log(brito);
