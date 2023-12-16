240369236e1f8cf1ad1bfbf27c748f0ceddee2a0
tocattabrito <tocattabrito@gmail.com>
2023-12-15 21:07:44 -0300

    Listas (array)

---
+++ 06 - listas (array)/aula.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //  notas do aluno
+
+// const notas = [];
+
+// notas.push(5);
+// notas.push(7);
+// notas.push(8);
+// notas.push(2);
+// notas.push(5);
+
+// const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4];
+// console.log(soma / notas.length);
+
+
+
+
+
+
+
+
+
+
+
+
+// // lista
+
+// const alunos = ['João', 'Vitor', 'Marina'];
+
+
+// // adicionar elemento a lista
+// alunos.push('Renan'); // insere no final da lista
+// alunos[4] = 'Vinícius';
+
+
+// // Saída
+// console.log(alunos);
+
