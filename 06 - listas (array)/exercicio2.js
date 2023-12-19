2a07705f3afb03088845d7312707f3d6520c20ef
tocattabrito <tocattabrito@gmail.com>
2023-12-18 21:01:56 -0300

    Update

--- 06 - listas (array)/exercicio2.js
+++ 06 - listas (array)/exercicio2.js
@@ -4,10 +4,15 @@
 const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 
-for (let i = 0; i <= 10; i++) {
-    
-    if (numeros % 2 === 0) {
-        console.log(numeros[i]);
-    }
+for (let i = 0; i < numeros.length; i++) {
+   
+    const numero = numeros[i];
+    const numeroPar = (numero % 2) === 0;
+   
+    if (numeroPar) {
+       console.log([i]+' Par');
+   } else {
+    //    console.log([i]+' Impar')
+   }
         
 }
