aa42ddeee8269680c56adb639a679e0ec0950cab
tocattabrito <tocattabrito@gmail.com>
2023-12-18 21:22:03 -0300

    Update

--- 06 - listas (array)/exercicio2.js
+++ 06 - listas (array)/exercicio2.js
@@ -1,16 +1,15 @@
 // 2 - Crie um programa que seja capaz de percorrer uma lista de números
 // e imprima cada número par encontrado
 
-const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numeros = [26,13,10]; // lista
 
 
 for (let i = 0; i < numeros.length; i++) {
    
     const numero = numeros[i];
-    const numeroPar = (numero % 2) === 0;
-   
-    if (numeroPar) {
-       console.log([i]+' Par');
+  
+    if (numero % 2 === 0) {
+       console.log(numero +' Par');
    } else {
     //    console.log([i]+' Impar')
    }
