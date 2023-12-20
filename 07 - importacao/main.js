3cfd399ef3cc1acf8021101042bd5b942174861c
tocattabrito <tocattabrito@gmail.com>
2023-12-19 21:52:21 -0300

    Update

--- 07 - importacao/main.js
+++ 07 - importacao/main.js
@@ -1,3 +1,11 @@
 const funcoes = require('./funcoes_auxiliares'); // import
 
 console.log(funcoes.gets());
+
+/*
+Tem a mesma saída da função acima
+
+const {gets, print} = require('./funcoes_auxiliares');
+
+print(gets());
+*/ 
