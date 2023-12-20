346c95c86b9acbc53f45531c9c63da12a1dd2919
tocattabrito <tocattabrito@gmail.com>
2023-12-20 17:03:38 -0300

    Update

--- 07 - importacao/exercicio1.js
+++ 07 - importacao/exercicio1.js
@@ -15,9 +15,24 @@
 
 const { gets, print } = require('./funcoes_auxiliares');
 
-print(gets());
-print(gets());
-print(gets());
-print(gets());
-print(gets());
+const numerosSorteados = [];
 
+for (let i = 0; i < 5; i++) {
+    const numeroSorteado = gets();
+    numerosSorteados.push(Number(numeroSorteado));
+    
+}
+
+print(numerosSorteados);
+
+// exibe o maior número do array
+let maiorValor = 0;
+
+for (let i = 0; i < numerosSorteados.length; i++){
+    const numeroSorteado = numerosSorteados[i];
+    if (numeroSorteado > maiorValor) {
+        maiorValor = numeroSorteado;
+    }
+}
+
+print( maiorValor + ' É o maior valor sorteado');
