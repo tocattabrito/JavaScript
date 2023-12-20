59e3e075e9cde3cc107882894da0f40d543c40f6
tocattabrito <tocattabrito@gmail.com>
2023-12-20 16:41:01 -0300

    Exercício -  Maior número sorteado

---
+++ 07 - importacao/exercicio1.js
@@ -0,0 +1,23 @@
+/*
+    Uma sala contem 5 aulos cada aluno foi sorteado um número de 1 - 100.
+    Faça um programa que receba 5 números sorteados para os alunos  
+    mostre o maior valor sorteado.
+
+    Dados de entrada:
+    5
+    10
+    98
+    23
+
+    Saída
+    98
+*/
+
+const { gets, print } = require('./funcoes_auxiliares');
+
+print(gets());
+print(gets());
+print(gets());
+print(gets());
+print(gets());
+
--- 07 - importacao/funcoes_auxiliares.js
+++ 07 - importacao/funcoes_auxiliares.js
@@ -1,7 +1,13 @@
-function gets() {
-    return 10;
-}
 
+const entradas = [5, 50, 10, 98, 23];
+let i = 0;
+
+function gets() {
+    const valor = entradas[i];
+    i = i + 1;
+    return valor;
+}
+ 
 function print(texto) {
     console.log(texto);
 }
