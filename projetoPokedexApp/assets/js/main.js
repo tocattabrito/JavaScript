f4b3a4864d8277ff02bdb5770cac015fc9910057
tocattabrito <tocattabrito@gmail.com>
2023-12-25 09:01:24 -0300

    Update

--- projetoPokedexApp/assets/js/main.js
+++ projetoPokedexApp/assets/js/main.js
@@ -3,6 +3,8 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
+// Método assicrono "quando ocorrer" exiba a respota
 fetch(url).then(function (response) {
     console.log(response);
+    
 });
