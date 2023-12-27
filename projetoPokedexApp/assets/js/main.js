afc712e14e2b0e6d54e79db796ffd52918811665
tocattabrito <tocattabrito@gmail.com>
2023-12-27 18:00:37 -0300

    Update

--- projetoPokedexApp/assets/js/main.js
+++ projetoPokedexApp/assets/js/main.js
@@ -4,21 +4,13 @@ const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 // Método assicrono "quando ocorrer" exiba a respota
-/*Utilizado o arrow function =>*/ 
 
+/*Utilizado o arrow function => em uma função de call back*/ 
 fetch(url)
-    .then((response) => {
-        return response.json()
-    })
-    .then(function (jsonbody) {
-        console.log(jsonbody);
-    })
-    .catch(function (error) {
-        console.error(error);
-    })
-    .finally(function () {
-        console.log('Requisição concluida!');
-    });
+    .then((response) => response.json())
+    .then((jsonbody) => console.log(jsonbody))   
+    .catch((error) => console.log(error))
+ 
 
 /*Retorna o mesmo resultado do código acima*/ 
 // fetch(url)
