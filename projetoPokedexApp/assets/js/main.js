9ce51f367a6b4b5f9f026911d4fc9da8d20e1941
tocattabrito <tocattabrito@gmail.com>
2023-12-28 04:45:14 -0300

    Update

--- projetoPokedexApp/assets/js/main.js
+++ projetoPokedexApp/assets/js/main.js
@@ -27,13 +27,21 @@ function convertPokemonToLi(pokemon) {
 const pokemonList = document.getElementById('pokemonList');
 
                                               
-pokeApi.getPokemons().then((pokemons) => {
-    const listItens = [];
-    for (let i = 0; i < pokemons.length; i++) {
-        const pokemon = pokemons[i];
-        listItens.push(convertPokemonToLi(pokemon));
-    }
-    console.log(listItens);
+pokeApi.getPokemons().then((pokemons = []) => {
+    
+    const newList =  pokemons.map((value, index, array) => {
+        return "";
+    });
+        
+    
+    
+    
+    // const listItens = [];
+    // for (let i = 0; i < pokemons.length; i++) {
+    //     const pokemon = pokemons[i];
+    //     listItens.push(convertPokemonToLi(pokemon));
+    // }
+    // console.log(listItens);
 })
 
 /*Retorna o mesmo resultado do código acima*/
