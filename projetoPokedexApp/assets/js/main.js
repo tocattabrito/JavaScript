336de8334ef261d78065de47bcd5d61fda693ecc
tocattabrito <tocattabrito@gmail.com>
2023-12-28 00:45:19 -0300

    Update

--- projetoPokedexApp/assets/js/main.js
+++ projetoPokedexApp/assets/js/main.js
@@ -3,12 +3,41 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            
+        
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                    alt="${pokemon.name}">
+            </div>
+        </li>
+        
+    `    
+}
+
+
 // Método assicrono "quando ocorrer" exiba a respota
 
 /*Utilizado o arrow function => em uma função de call back*/ 
 fetch(url)
     .then((response) => response.json())
-    .then((jsonbody) => console.log(jsonbody))   
+    .then((jsonbody) => jsonbody.results)
+    .then((pokemonlist) => {
+      
+        for (let i = 0; i < pokemonlist.length; i++) {
+            const pokemon = pokemonlist[i];
+            console.log(convertPokemonToLi(pokemon));
+        }
+    })   
     .catch((error) => console.log(error))
  
 
