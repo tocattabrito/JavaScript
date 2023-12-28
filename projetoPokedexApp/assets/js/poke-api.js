f769c8f570ecf549174534de8bc74056da96312a
tocattabrito <tocattabrito@gmail.com>
2023-12-28 11:54:58 -0300

    Update

--- projetoPokedexApp/assets/js/poke-api.js
+++ projetoPokedexApp/assets/js/poke-api.js
@@ -8,3 +8,14 @@ pokeApi.getPokemons = (offset = 0, limit = 10) => {
         .then((jsonbody) => jsonbody.results)
         .catch((error) => console.error(error))
 } 
+
+Promise.all([
+    fetch('https://pokeapi.co/api/v2/pokemon/1'),
+    fetch('https://pokeapi.co/api/v2/pokemon/2'),
+    fetch('https://pokeapi.co/api/v2/pokemon/3'),
+    fetch('https://pokeapi.co/api/v2/pokemon/4')
+]).then((results) => {
+    console.log(results);
+}); 
+    
+
