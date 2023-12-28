2bd4add97a1b1e0036b98c60361d971d3bdf5e82
tocattabrito <tocattabrito@gmail.com>
2023-12-28 05:29:31 -0300

    Criação de lista e conversão em lista html

--- projetoPokedexApp/assets/js/main.js
+++ projetoPokedexApp/assets/js/main.js
@@ -1,9 +1,4 @@
 
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
-
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
@@ -28,23 +23,14 @@ const pokemonList = document.getElementById('pokemonList');
 
                                               
 pokeApi.getPokemons().then((pokemons = []) => {
-    
-    const newList =  pokemons.map((pokemon) => {
-        return convertPokemonToLi(pokemon);
-    });
-    
-    const newHtml = newList.join('');   
-   
-    pokemonList.innerHTML += newHtml;
-    
-    
-    // const listItens = [];
-    // for (let i = 0; i < pokemons.length; i++) {
-    //     const pokemon = pokemons[i];
-    //     listItens.push(convertPokemonToLi(pokemon));
-    // }
-    // console.log(listItens);
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
 })
+
+
+
+
+
+
 
 /*Retorna o mesmo resultado do código acima*/
 // fetch(url)
