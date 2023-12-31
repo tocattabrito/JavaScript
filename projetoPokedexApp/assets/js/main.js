fc1e7aa9c60c79a1b1de9d50b8b50afeea35b06c
tocattabrito <tocattabrito@gmail.com>
2023-12-31 09:35:12 -0300

    Update

--- projetoPokedexApp/assets/js/main.js
+++ projetoPokedexApp/assets/js/main.js
@@ -1,5 +1,5 @@
 const pokemonList = document.getElementById('pokemonList')
-// const loadMoreButton = document.getElementById('loadMoreButton')
+const loadMoreButton = document.getElementById('loadMoreButton')
 
 const maxRecords = 151;
 const limit = 10;
@@ -25,12 +25,32 @@ function convertPokemonToLi(pokemon) {
     `    
 }
 
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
 
+loadPokemonItens(offset, limit)
 
-                                              
-pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qrdRecordWithNextPage = offset + limit
+
+    if (qrdRecordWithNextPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)   
+    } else {
+        loadPokemonItems(offset, limit)
+    }
 })
+                                              
+// pokeApi.getPokemons().then((pokemons = []) => {
+//     pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
+// })
 
 
 
