ab83c155f90b70eece2822ef664680c264d5b232
tocattabrito <tocattabrito@gmail.com>
2023-12-30 21:52:24 -0300

    Update Código completo do arquivo poke.api

--- projetoPokedexApp/assets/js/poke-api.js
+++ projetoPokedexApp/assets/js/poke-api.js
@@ -1,6 +1,28 @@
 
 const pokeApi = [];
 
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprite.other.dream_world.front_default
+
+    return pokemon
+}
+
+pokeApi.getPokemonsDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
